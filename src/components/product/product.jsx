0f69a63daf65867fe7f2d96e1d7008cc8e004067
Houlgatte Louka <bazar.tri@gmail.com>
2020-02-14 14:50:41 +0100

    change picture size

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './product.css';
 
 export const Product = props => {
     return (
         <>
-            <div><img src={props.data.url}/></div>
+            <div><img src={props.data.url} alt={props.data.description} className={"product"}/></div>
             <div>Marque : {props.data.brand}</div>
             <div>Description : {props.data.description}</div>
             <div>Prix : {props.data.price}€</div>
